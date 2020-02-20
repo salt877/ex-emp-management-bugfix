faa7d8fc5f36c10e85c601812e799764a70b0fbc
shiodarina <rina.shioda@rakus-partners.co.jp>
2020-02-20 15:44:19 +0900

    システム内で処理されなかった例外を処理して500エラーページを表示するクラスを作成しました。

--- src/main/java/jp/co/sample/emp_management/common/GlobalExceptionHandler.java
+++ src/main/java/jp/co/sample/emp_management/common/GlobalExceptionHandler.java
@@ -5,13 +5,15 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler implements HandlerExceptionResolver {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object obj,
+	@Override
+	public ModelAndView resolveException (HttpServletRequest request, HttpServletResponse response, Object obj,
 			Exception e) {
 		LOGGER.error("システムエラーが発生しました！", e);
 		return null;

5c42d1be0bf835224ad7520f398ea3b5f877b9fb
shiodarina <rina.shioda@rakus-partners.co.jp>
2020-02-20 15:59:29 +0900

    (1-5)確認用パスワード欄を設置してパスワードと一致したときに登録できるよう改修しました。

--- src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
+++ src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
@@ -10,36 +10,53 @@ import javax.validation.constraints.NotBlank;
  */
 public class InsertAdministratorForm {
 	/** 名前 */
-	@NotBlank(message="名前は必須です")
+	@NotBlank(message = "名前は必須です")
 	private String name;
 	/** メールアドレス */
-	@NotBlank(message="メールアドレスは必須です")
+	@NotBlank(message = "メールアドレスは必須です")
 	private String mailAddress;
 	/** パスワード */
-	@NotBlank(message="パスワードは必須です")
+	@NotBlank(message = "パスワードは必須です")
 	private String password;
+	/** 確認用パスワード */
+	@NotBlank(message = "確認用パスワードは必須です")
+	private String checkPassword;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getMailAddress() {
 		return mailAddress;
 	}
+
 	public void setMailAddress(String mailAddress) {
 		this.mailAddress = mailAddress;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public String getCheckPassword() {
+		return checkPassword;
+	}
+
+	public void setCheckPassword(String checkPassword) {
+		this.checkPassword = checkPassword;
+	}
+
 	@Override
 	public String toString() {
 		return "InsertAdministratorForm [name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
-				+ "]";
+				+ ", checkPassword=" + checkPassword + "]";
 	}
-	
 }
